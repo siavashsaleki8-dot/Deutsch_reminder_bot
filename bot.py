be5a3778c9cd80b7b405a63f05cff640a5637fbc
siavashsaleki8-dot <siavashsaleki8@gmail.com>
2026-07-07 13:30:55 +0330

    Update bot security

--- bot.py
+++ bot.py
@@ -1,15 +1,17 @@
+import os
 import requests
 import random
 
-TOKEN = 8629996047:AAHddPLE0z8_VVHimoWp58E1-5AUJ3So3Hw
-CHAT_ID = "-4753385536"
+TOKEN = os.environ[8629996047:AAHddPLE0z8_VVHimoWp58E1-5AUJ3So3Hw ]
+CHAT_ID = os.environ["CHAT_ID"]
 
 topics = [
     "Mein Wochenende",
     "Meine Familie",
     "Meine Arbeit",
     "Meine Reise",
-    "Meine Ziele"
+    "Meine Ziele",
+    "Mein Lieblingsfilm"
 ]
 
 topic = random.choice(topics)
@@ -19,7 +21,7 @@ message = f"""🇩🇪 Zeit zum Sprechen!
 Thema des Tages:
 {topic}
 
-Sprich 2 Minuten Deutsch und sende eine Sprachnachricht 🎤
+Sprich 2 Minuten auf Deutsch und sende eine Sprachnachricht 🎤
 """
 
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
