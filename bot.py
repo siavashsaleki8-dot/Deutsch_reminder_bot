592adaa058918d06661e82b51569259fad204c0f
siavashsaleki8-dot <siavashsaleki8@gmail.com>
2026-07-07 13:41:56 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 import requests
 import random
 
-TOKEN = os.environ[8629996047:AAHddPLE0z8_VVHimoWp58E1-5AUJ3So3Hw ]
+TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
 
 topics = [
